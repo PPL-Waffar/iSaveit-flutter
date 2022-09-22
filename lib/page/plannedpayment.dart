7db0bf15e451c4921326e4492cc5e6893a62d0a7
Anne.yuliana <anne.yuliana@ui.ac.id>
2022-09-22 08:14:42 +0700

    Create front-end for planned payment feature

--- lib/page/plannedpayment.dart
+++ lib/page/plannedpayment.dart
@@ -1,46 +1,46 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class PlannedPayment extends StatefulWidget {
-//   @override
-//   State<StatefulWidget> createState() {
-//     return PlannedPaymentState();
-//   }
-// }
+class PlannedPayment extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return PlannedPaymentState();
+  }
+}
 
-// class PlannedPaymentState extends State<PlannedPayment> {
-//   String _paymentname;
-//   String _expense;
+class PlannedPaymentState extends State<PlannedPayment> {
+  String _paymentname;
+  String _expense;
 
-//   final GlobalKey<PlannedState> _plannedKey = GlobalKey<PlannedState>();
+  final GlobalKey<PlannedState> _plannedKey = GlobalKey<PlannedState>();
 
-//   Widget _buildPaymentName() {
-//     return null;
-//   }
+  Widget _buildPaymentName() {
+    return null;
+  }
 
-//   Widget _buildExpense() {
-//     return null;
-//   }
+  Widget _buildExpense() {
+    return null;
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text("Planned Payment")),
-//       body: Container(
-//           margin: EdgeInsets.all(24),
-//           child: Form(
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: <Widget>[
-//                 _buildPaymentName(),
-//                 _buildExpense(),
-//                 SizedBox(height: 100),
-//                 RaisedButton(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Planned Payment")),
+      body: Container(
+          margin: EdgeInsets.all(24),
+          child: Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildPaymentName(),
+                _buildExpense(),
+                SizedBox(height: 100),
+                RaisedButton(
                   
-//                   onPressed: () => {},
-//                 )
-//               ],
-//             ),
-//           )),
-//     );
-//   }
-// }
+                  onPressed: () => {},
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
