41131b74d959e77396c497450eb6e227e2e5dffb
Reynaldi Oktavianus <reynaldi@Reynaldis-MacBook-Pro.local>
2022-10-03 00:39:10 +0700

    [GREEN] Adding test case

--- test/profile_test.dart
+++ test/profile_test.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:isaveit/page/homepage.dart';
 import 'package:isaveit/page/profile.dart';
 
 void main() {
   testWidgets('Testing homepage', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MaterialApp(
+    await tester.pumpWidget(const MaterialApp(
       home: ProfileView(),
     ));
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,19 +15,19 @@ import 'package:isaveit/page/homepage.dart';
 void main() {
   testWidgets('Testing homepage', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MaterialApp(
+    await tester.pumpWidget(const MaterialApp(
       home: HomeView(),
     ));
 
-    // Verify that our counter starts at 0.
+    
     expect(find.byIcon(Icons.add), findsOneWidget);
     expect(find.text('Welcome Back! \nYourname'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
+    
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
+    
     expect(find.text('Welcome Back! \nYourname'), findsOneWidget);
 
     expect(find.text('Welcome Back!'), findsNothing);
