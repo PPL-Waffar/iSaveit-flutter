4d92f86e020e1b79fd2b6cd094087e7c5aa70d5f
Anne.yuliana <anne.yuliana@ui.ac.id>
2022-10-15 18:01:59 +0700

    [GREEN] Connect Planned Payment with Heroku URL

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -8,16 +8,18 @@ import 'package:flutter/scheduler.dart';
 import 'dart:async';
 import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+//url heroku: "https://isaveit-staging.herokuapp.com/user/flu-login/"
+//url local: "http://127.0.0.1:8000/user/flu-login/"
 
 class Login extends StatefulWidget {
   const Login({super.key});
   Future<User> webServiceLogin(String telephone, String password) async {
     var response =
-        await post(Uri.parse("https://isaveit-staging.herokuapp.com/user/flu-login/"),
-            headers: <String, String>{
-              'Content-Type': 'application/json; charset=UTF-8',
-            },
-            body: jsonEncode({"email": telephone, "password": password}));
+    await post(Uri.parse("https://isaveit-staging.herokuapp.com/user/flu-login/"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode({"email": telephone, "password": password}));
 
     if (response.statusCode == 200) {
       Map<String, dynamic> userData = jsonDecode(response.body);
@@ -57,129 +59,133 @@ class LoginPage extends State<Login> {
       ),
       body: SingleChildScrollView(
           child: Column(children: <Widget>[
-        const SizedBox(
-          height: 20,
-        ),
-        const Text(
-          'Login to your account',
-          style: TextStyle(fontSize: 24, color: Color(0xff3444CE)),
-        ),
-        const SizedBox(
-          height: 7,
-        ),
-        const Text(
-          "Welcome back, you've been missed",
-          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        const Text(
-          'Email',
-          textAlign: TextAlign.left,
-          style: TextStyle(fontSize: 14),
-        ),
-        Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    key: const Key("addEmail"),
-                    controller: _email,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter your name'),
-                  ),
-                ),
-              ],
+            const SizedBox(
+              height: 20,
             ),
-          ),
-        
-        const Text(
-          'Password',
-          style: TextStyle(fontSize: 14),
-        ),
-        Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    key: const Key("addPassword"),
-                    controller: _password,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter your password'),
-                  ),
-                ),
-              ],
+            const Text(
+              'Login to your account',
+              style: TextStyle(fontSize: 24, color: Color(0xff3444CE)),
             ),
-          ),
-        
-        const SizedBox(
-          height: 25,
-        ),
-        Container(
-          height: 48,
-          width: 327,
-          padding: const EdgeInsets.fromLTRB(70, 16, 70, 16),
-          decoration: const BoxDecoration(color: Color(0xff4054FF)),
-          child: ElevatedButton(
-            key: const Key("loginAccount"),
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size.fromHeight(48),
-              elevation: 0,
-              backgroundColor: const Color(0XFF4054FF),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
+            const SizedBox(
+              height: 7,
+            ),
+            const Text(
+              "Welcome back, you've been missed",
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Email',
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: 14),
+            ),
+            Card(
+              margin: const EdgeInsets.all(20),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        key: const Key("addEmail"),
+                        controller: _email,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Enter your name'),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            onPressed: () async {
-              setState(() {
-                loading = true;
-              });
-              {
-                User user =
+            const Text(
+              'Password',
+              style: TextStyle(fontSize: 14),
+            ),
+            Card(
+              margin: const EdgeInsets.all(20),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        key: const Key("addPassword"),
+                        controller: _password,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Enter your password'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              height: 48,
+              width: 327,
+              padding: const EdgeInsets.fromLTRB(70, 16, 70, 16),
+              decoration: const BoxDecoration(color: Color(0xff4054FF)),
+              child: ElevatedButton(
+                key: const Key("loginAccount"),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(48),
+                  elevation: 0,
+                  backgroundColor: const Color(0XFF4054FF),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                onPressed: () async {
+                  setState(() {
+                    loading = true;
+                  });
+                  {
+                    User user =
                     await widget.webServiceLogin(_email.text, _password.text);
 
-                {
-                  SchedulerBinding.instance.addPostFrameCallback((_) {
-                    Navigator.of(context).pushAndRemoveUntil(
-                        MaterialPageRoute<void>(
-                            builder: (BuildContext context) =>
-                                SettingView(user)),
-                        (Route<dynamic> route) => false);
-                  });
-                }
-              }
-              _email.clear();
-              _password.clear();
-            },
-            child: const Text(
-              'Login',
-              style: TextStyle(fontSize: 16, color: Colors.white),
+                    {
+                      SchedulerBinding.instance.addPostFrameCallback((_) {
+                        Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute<void>(
+                                builder: (BuildContext context) =>
+                                    SettingView(user)),
+                                (Route<dynamic> route) => false);
+                      });
+                    }
+                  }
+                  _email.clear();
+                  _password.clear();
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
             ),
-          ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        TextButton(
-          // <-- TextButton
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const Register()));
-          },
-          child: const Text(
-            "Don't have an account?",
-            style: TextStyle(
-              color: Color(0XFF4054FF),
+            const SizedBox(
+              height: 20,
             ),
-          ),
-        ),
-      ])),
+            TextButton(
+              // <-- TextButton
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Register()));
+              },
+              child: const Text(
+                "Don't have an account?",
+                style: TextStyle(
+                  color: Color(0XFF4054FF),
+                ),
+              ),
+            ),
+          ])),
     );
   }
 }
