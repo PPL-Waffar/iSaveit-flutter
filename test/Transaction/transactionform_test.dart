9a09e1af8e66750732d060cd9e9401f45ee79928
anastasia.audi <anastasia.audi@ui.ac.id>
2022-10-22 10:25:30 +0700

    [REFACTOR] Fix Transaction Form Test Cases

--- test/Transaction/transactionform_test.dart
+++ test/Transaction/transactionform_test.dart
@@ -20,7 +20,7 @@ void main() {
         sessionId: "1234567890",
         isCitizen: true,
         email: "usertest@gmail.com",
-        name: "Amanda");
+        name: "User One");
 
     final transactionName = find.byKey(const ValueKey("transactionName"));
     final transactionAmount = find.byKey(const ValueKey("transactionAmount"));
@@ -39,7 +39,7 @@ void main() {
     expect(find.text("Type of Payment"), findsOneWidget);
 
     await tester.pump();
-
+    
     await tester.enterText(transactionName, "Jajan kantin nasi goreng");
     await tester.pumpAndSettle();
     await tester.enterText(transactionAmount, "30.000");
@@ -47,43 +47,42 @@ void main() {
 
     await tester.pump(const Duration(seconds: 2));
 
-
     await tester.enterText(transactionDate, "2022-10-02");
     final dateTextField = find.byIcon(Icons.calendar_today);
         await tester.tap(dateTextField);
  
-    // //Test transaction type widget
-    // final transacDropdown = find.byKey(const ValueKey('transacType'));
-    //   await tester.tap(transacDropdown);
-    //   await tester.pumpAndSettle();
+    //Test transaction type widget
+    final transacDropdown = find.byKey(const ValueKey('transactionType'));
+      await tester.tap(transacDropdown);
+      await tester.pumpAndSettle();
 
-    // ///if you want to tap first item
-    // final transacItem = find.text('Income').last;
+    ///if you want to tap first item
+    final transacItem = find.text('Income').last;
 
-    // await tester.tap(transacItem);
-    // await tester.pumpAndSettle();
+    await tester.tap(transacItem);
+    await tester.pumpAndSettle();
 
-    //     //Test transaction type widget
-    // final paymentDropdown = find.byKey(const ValueKey('paymentType'));
-    // await tester.tap(paymentDropdown);
-    // await tester.pumpAndSettle();
+    // Test transaction type widget
+    final paymentDropdown = find.byKey(const ValueKey('PaymentType'));
+      await tester.tap(paymentDropdown);
+      await tester.pumpAndSettle();
 
-    // ///if you want to tap first item
-    // final paymentItem = find.text('Debit').last;
+    ///if you want to tap first item
+    final paymentItem = find.text('debit card').last;
 
-    // await tester.tap(paymentItem);
-    // await tester.pumpAndSettle();
+    await tester.tap(paymentItem);
+    await tester.pumpAndSettle();
 
     //Test button
-    // expect(find.byType(ElevatedButton), findsOneWidget);
-    // await tester.pump(const Duration(milliseconds: 100));
-    // await tester.tap(find.byType(ElevatedButton));
-    // await tester.pumpAndSettle();
+    expect(find.byType(ElevatedButton), findsOneWidget);
+    await tester.pump(const Duration(milliseconds: 100));
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pumpAndSettle();
 
-    // expect(find.byType(TextButton), findsOneWidget);
-    // await tester.pump(const Duration(milliseconds: 100));
-    // await tester.tap(find.byType(TextButton));
-    // await tester.pumpAndSettle();
+    expect(find.byType(TextButton), findsOneWidget);
+    await tester.pump(const Duration(milliseconds: 100));
+    await tester.tap(find.byType(TextButton));
+    await tester.pumpAndSettle();
 
   });
 
