5484b9f5bea2a0fe41de6fb3bcd47072e9321a1e
Reynaldi Oktavianus <reynaldi@Reynaldis-MacBook-Pro.local>
2022-10-24 18:28:11 +0700

    [GREEN] Remove unused SonarCloud"

--- test/report_test.dart
+++ test/report_test.dart
@@ -3,15 +3,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:isaveit/page/report/reportpage.dart';
 
 void main() {
+  
   testWidgets('Testing Report page', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MaterialApp(
       home: ReportView(),
     ));
+
+    
+  
     expect(find.text("My Spendings"), findsOneWidget);
     expect(find.text('September 2022'), findsOneWidget);
     expect(find.text('Overview'), findsOneWidget);
     expect(find.text("This month's spending"), findsOneWidget);
-
   });
 }
