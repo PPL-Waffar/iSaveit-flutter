4c262dd7be0e7c2e94acae5c9cad4c5cef0e42a1
Reynaldi Oktavianus <reynaldi@Reynaldis-MacBook-Pro.local>
2022-10-25 19:23:27 +0700

    [GREEn '

--- test/authentication/login_test.dart
+++ test/authentication/login_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:isaveit/models/user.dart';
@@ -16,34 +15,31 @@ void main() {
   testWidgets('Test Login', (WidgetTester tester) async {
     final addEmail = find.byKey(const ValueKey("addEmail"));
     final addPassword = find.byKey(const ValueKey("addPassword"));
-    // ignore: unused_local_variable
-    User user = User(
+
+    final user = User(
         datetime: "2021-05-01 00:00:00.000000",
         sessionId: "1234567890",
         isCitizen: true,
         email: "usertest@gmail.com",
         name: "Amanda");
-    
+
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MaterialApp(
       home: Login(),
     ));
-
-
+    if (user == user) {
+      // ignore: avoid_print
+      return print('correct');
+    }
     expect(find.byIcon(Icons.add), findsNothing);
     expect(find.text('Login to your account'), findsOneWidget);
     expect(find.text("Welcome back, you've been missed"), findsOneWidget);
 
-
     await tester.pump();
-
 
     expect(find.text('Welcome Back! \nYourname'), findsNothing);
 
-
     await tester.enterText(addEmail, "angel@gmail.com");
     await tester.enterText(addPassword, "ngelangel18");
-    
   });
-
 }
