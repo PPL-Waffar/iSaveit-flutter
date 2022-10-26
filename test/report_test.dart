4e598b8eac07076514597097bbfcfb9bc782b53d
Anne.yuliana <anne.yuliana@ui.ac.id>
2022-10-26 22:10:39 +0700

    [GREEN] Fix test cases

--- test/report_test.dart
+++ test/report_test.dart
@@ -5,9 +5,22 @@ import 'package:isaveit/page/report/reportpage.dart';
 void main() {
   testWidgets('Testing Report page', (WidgetTester tester) async {
     // Build our app and trigger a frame.
+    final previousReport = find.byKey(const ValueKey("previousPage"));
+    final nextReport = find.byKey(const ValueKey("nextPage"));
+
     await tester.pumpWidget(const MaterialApp(
       home: ReportView(),
     ));
-    expect(find.text("Your Report"), findsOneWidget);
+
+    expect(find.text("My Spendings"), findsOneWidget);
+    expect(find.text('September 2022'), findsOneWidget);
+    expect(find.text('Overview'), findsOneWidget);
+
+
+    await tester.tap(previousReport);
+    await tester.tap(nextReport);
+    await tester.pump();
+    expect(find.text("This month's spending"), findsOneWidget);
+
   });
 }
