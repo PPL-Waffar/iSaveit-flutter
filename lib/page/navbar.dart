712018ec27af37dc89b28404ba79581863d6cf71
anastasia.audi <anastasia.audi@ui.ac.id>
2022-10-27 07:47:09 +0700

    [REFACTOR] Connect Profile page to Navbar

--- lib/page/navbar.dart
+++ lib/page/navbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:isaveit/models/user.dart';
+import 'package:isaveit/page/newsletter.dart';
 import 'package:isaveit/page/profile/profile_detail.dart';
 import '../page/homepage.dart';
 import '../page/report/reportpage.dart';
@@ -29,6 +30,7 @@ class _SettingViewState extends State<SettingView> {
       HomeView(widget.user),
       const ReportView(),
       Transaction(widget.user),
+      const NewsView(),
       const ProfileView()
     ];
   }
