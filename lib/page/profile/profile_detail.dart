c751027da3a318ec40279fb733062bab33bbc4b3
anastasia.audi <anastasia.audi@ui.ac.id>
2022-11-07 18:22:22 +0700

    [RED]Add navigation to satisfaction Report in Profile

--- lib/page/profile/profile_detail.dart
+++ lib/page/profile/profile_detail.dart
@@ -198,25 +198,60 @@ class _ProfileDetailPage extends State<ProfileView> {
                             ])),
                     
                     const SizedBox(height: 32),
-
+                    Padding(padding: const EdgeInsets.only(left: 30, right: 30),
+                    child:
                     Container(
-                      alignment: Alignment.center,
-                      margin: const EdgeInsets.only(left: 30, right: 30),
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              minimumSize: const Size.fromHeight(48),
-                              elevation: 0,
-                              backgroundColor: const Color(0XFF4054FF),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(48),
-                              )),
-                          onPressed: () => 
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => const CreateFeedback())), 
-                          child: const Text('Create Feedback'),
-                      )
-                    ),
-                
+                    decoration: BoxDecoration(
+                    color: const Color(0xFFDFE2FF),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
 
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        //add text 
+                        Container(
+                          alignment: Alignment.topLeft,
+                          margin: const EdgeInsets.only(left: 30, right: 30, top: 20),
+                          child: const Text('Set your goals and satisfaction here! ',
+                            style: TextStyle(
+                              fontFamily: 'Inter',
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF4054FF),
+                            ),
+                          ),
+                        ),
+
+                        Container(
+                          alignment: Alignment.topLeft,
+                          margin: const EdgeInsets.only(left: 30, right: 30, top: 20),
+                          child: const Text('Keep track of your monthly progress and see how you’ve been managing your money!',
+                            style: TextStyle(
+                              fontFamily: 'Inter',
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500
+                            ),
+                          ),
+                        ),
+
+                        //add button to create_feedback.dart
+                        Container(
+                          alignment: Alignment.bottomRight,
+                          margin: const EdgeInsets.only(left: 30, right: 30, top: 20),
+                          child: TextButton(
+                              onPressed: () => 
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const CreateFeedback())), 
+                              child: const Text('Click here to see more', style:TextStyle(color: Color(0xFF4054FF),)),
+                          )
+                        ),
+
+                        const SizedBox(height: 5),
+                      ],
+                    )
+                    )
+                    ),
+                    const SizedBox(height: 32),
                   ]))
                     ),
                   );
