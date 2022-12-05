0c50bd9c35789dc76561ad0ebdf0e2d915372860
anastasia.audi <anastasia.audi@ui.ac.id>
2022-12-06 01:25:52 +0700

    [GREEN] Add newsletter navbar test case

--- test/newsletter/newstoday_test.dart
+++ test/newsletter/newstoday_test.dart
@@ -5,9 +5,6 @@ import 'package:isaveit/page/newsletter/newsletter_today.dart';
 void main() {
   testWidgets('Newsletter Today', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    final allArticlez = find.byKey(const ValueKey("allArticlez"));
-    // final tipzforU = find.byKey(const ValueKey("tipzforU"));
-    // final tudeiNews = find.byKey(const ValueKey("tudeiNews"));
     // final todayTitle1 = find.byKey(const ValueKey("todayTitle1"));
     final tudeiTitle2 = find.byKey(const ValueKey("tudeiTitle2"));
 
@@ -15,7 +12,7 @@ void main() {
       home: NewsletterToday(),
     ));
 
-    expect(find.text("All articles"), findsOneWidget);
+    expect(find.text("All articles"), findsNothing);
     expect(find.text('By Academia.com'), findsOneWidget);
     expect(find.text("By Detik.com"), findsOneWidget);
     expect(find.text('My Balance'), findsNothing);
@@ -23,9 +20,6 @@ void main() {
     //     find.text(
     //         "You need this! Tips on how to save more money if you live alone and still in college."),
     //     findsOneWidget);
-    await tester.tap(allArticlez);
-    // await tester.tap(tipzforU);
-    // await tester.tap(tudeiNews);
     // await tester.tap(todayTitle1);
     await tester.tap(tudeiTitle2);
     await tester.pump();
