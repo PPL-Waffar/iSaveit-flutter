8242ceab4544c636bdb3048700c47deb53f24d3c
Anne.yuliana <anne.yuliana@ui.ac.id>
2022-12-30 11:12:39 +0700

    [GREEN] Remed creating pp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'page/register_page.dart';
+import 'package:isaveit/page/plannedpayment/plannedpayment.dart';
 
 
 void main() => runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Register(),
+      home: Plannedpayment(),
       
     );
   }
